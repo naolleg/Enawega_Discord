017e117bcc9dcad4d2f1d2448c0ce326d94a9bde
naolleg <naollegesse51@gmail.com>
2024-09-11 07:36:37 +0300

    added some frontend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 import express from "express"
 import cors from "cors"
+import { fileURLToPath } from 'url';
+import * as path from 'path';
+
 
 const app = express();
+
 app.use(express.json());
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.join(__dirname, "client")));
 app.use(cors({ origin: true }));
 
 const PORT=3001 || process.env.PORT;
+const HOST="localhost" || process.env.HOST;
 
 app.listen(PORT, () => console.log(`server runing on port ${PORT}`));
+console.log(`server is running on http://${HOST}:${PORT}`)

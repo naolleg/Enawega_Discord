b892e95f1f2e0573d12ac6a8ffb51b42cbfbc933
naolleg <naollegesse51@gmail.com>
2024-09-11 08:47:38 +0300

    making connection with socket.io

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import { fileURLToPath } from 'url';
 import * as path from 'path';
-
+import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -12,8 +15,23 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, "client")));
 app.use(cors({ origin: true }));
 
-const PORT=3001 || process.env.PORT;
-const HOST="localhost" || process.env.HOST;
 
-app.listen(PORT, () => console.log(`server runing on port ${PORT}`));
-console.log(`server is running on http://${HOST}:${PORT}`)
+io.on('connection', (socket) => {
+    console.log("new connection");
+    socket.emit('message','welcome to chat')
+
+    socket.broadcast.emit('message','user joined');
+  socket.on('disconnect',()=> {
+    io.emit('message','a user has left chat');
+  });
+
+  socket.on('ChatMessage',msg => {
+    io.emit('chat')
+  })
+});
+
+const PORT = 3001 || process.env.PORT;
+const HOST = "localhost" || process.env.HOST;
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+console.log(`server is running on http://${HOST}:${PORT}`);

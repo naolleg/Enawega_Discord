79f104df6347fdfdf61d918b7582a11bdb58c9ae
naolleg <naollegesse51@gmail.com>
2024-09-29 22:40:50 +0300

    some update on message format

--- server/src/utils/message.js
+++ server/src/utils/message.js
@@ -1,10 +1,8 @@
-import moment from 'moment';
 
 export default function formatMessage(username, text) {
   return {
     username,
-    text,
-    time: moment().format('h:mm a')
+    text
   };
 }
 
